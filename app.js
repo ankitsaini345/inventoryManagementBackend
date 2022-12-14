e986540e1dc9a16a9a671783135fc51ceb6e494b
Ankit Saini <ankit-a.saini@db.com>
2022-12-14 13:19:49 +0530

    frontend config in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const config = require('./config/config')
 const routes = require('./routes')
 
@@ -8,11 +9,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.status(200).send('Application is Running!');
-})
+// app.get('/', (req, res) => {
+//     res.status(200).send('Application is Running!');
+// })
 
 app.use(config.baseUrl, routes);
+
+app.use(express.static(path.join(__dirname, 'frontend', 'dist', 'inventory-management')));
+
+app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'inventory-management', 'index.html'))
+});
 
 const port = config.port || 4000;
 app.listen(port, (err) => {
